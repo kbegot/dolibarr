783607d0138eaccd7b3e709c0f4d8036bb18ebb3
Kévin Begot <kevinbegot@MacBook-Pro-de-Kevin.local>
2025-02-13 13:28:16 +0100

    [ADD] clean code

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -6193,15 +6193,12 @@ function dol_print_error($db = null, $error = '', $errors = null)
 			$syslog .= ", msg=".$msg;
 		}
 	}
-	if (empty($dolibarr_main_prod)
-		&& !empty($_SERVER['DOCUMENT_ROOT'])
-		&& function_exists('xdebug_call_file')
-		&& function_exists('xdebug_call_line')
-		&& function_exists('xdebug_call_function')) {
-		$out .= '<b>XDebug information:</b><br>' . "\n";
-		$out .= 'File: ' . xdebug_call_file() . '<br>' . "\n";
-		$out .= 'Line: ' . xdebug_call_line() . '<br>' . "\n";
-		$out .= 'Function: ' . xdebug_call_function() . '<br>' . "\n";
+	if (empty($dolibarr_main_prod) && !empty($_SERVER['DOCUMENT_ROOT']) && function_exists('xdebug_print_function_stack') && function_exists('xdebug_call_file')) {
+		xdebug_print_function_stack();
+		$out .= '<b>XDebug information:</b>'."<br>\n";
+		$out .= 'File: '.xdebug_call_file()."<br>\n";
+		$out .= 'Line: '.xdebug_call_line()."<br>\n";
+		$out .= 'Function: '.xdebug_call_function()."<br>\n";
 		$out .= "<br>\n";
 	}
 
